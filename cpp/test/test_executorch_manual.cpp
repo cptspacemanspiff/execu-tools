813eba534c033188ac4ebeb6b9e5785711dff15d
cptspacemanspiff <19273992+cptspacemanspiff@users.noreply.github.com>
2025-01-06 13:25:41 -0800

    Added etdump logger.

--- cpp/test/test_executorch_manual.cpp
+++ cpp/test/test_executorch_manual.cpp
@@ -6,13 +6,19 @@
 #include "executorch/runtime/core/result.h"
 #include "executorch/runtime/executor/program.h"
 #include <cassert>
+#include <executorch/runtime/platform/runtime.h>
 #include <iostream>
 #include <memory>
 #include <span>
 
+#include <executorch/devtools/etdump/etdump_flatcc.h>
+
 #include "ExecuToolsTestDirs.h"
 
 int main() {
+
+  executorch::runtime::runtime_init();
+
   // create a new executorch program.
   using executorch::aten::ScalarType;
   using executorch::aten::Tensor;
@@ -29,8 +35,8 @@ int main() {
   using executorch::runtime::Result;
   using executorch::runtime::Span;
 
-  Result<FileDataLoader> loader =
-      FileDataLoader::from(EXECUTOOLS_PYTHON_ARTIFACT_DIR "/stateful_model.pte");
+  Result<FileDataLoader> loader = FileDataLoader::from(
+      EXECUTOOLS_PYTHON_ARTIFACT_DIR "/stateful_model.pte");
   assert(loader.ok());
 
   Result<Program> program = Program::load(&loader.get());
@@ -52,7 +58,8 @@ int main() {
   std::vector<Span<uint8_t>> planned_arenas; // Passed to the allocator
 
   size_t num_memory_planned_buffers = method_meta->num_memory_planned_buffers();
-  std::cout << "num_memory_planned_buffers: " << num_memory_planned_buffers << std::endl;
+  std::cout << "num_memory_planned_buffers: " << num_memory_planned_buffers
+            << std::endl;
   // It is possible to have multiple layers in our memory hierarchy; for
   // example, SRAM and DRAM.
   for (size_t id = 0; id < num_memory_planned_buffers; ++id) {
@@ -65,7 +72,11 @@ int main() {
   HierarchicalAllocator planned_memory(
       {planned_arenas.data(), planned_arenas.size()});
 
-  auto memory_span =
+  auto memory_span_0 =
+      std::span<float>(reinterpret_cast<float *>(planned_arenas[0].data()),
+                       planned_arenas[0].size() / sizeof(float));
+
+  auto memory_span_1 =
       std::span<float>(reinterpret_cast<float *>(planned_arenas[1].data()),
                        planned_arenas[1].size() / sizeof(float));
 
@@ -77,7 +88,11 @@ int main() {
   // will use.
   MemoryManager memory_manager(&method_allocator, &planned_memory);
 
-  Result<Method> method = program->load_method(method_name, &memory_manager);
+
+  executorch::etdump::ETDumpGen etdump_gen{};
+  
+  Result<Method> method =
+      program->load_method(method_name, &memory_manager, &etdump_gen);
   assert(method.ok());
 
   // Create our input tensor.
@@ -101,5 +116,30 @@ int main() {
 
   EValue output = method->get_output(0);
 
+  auto output_address = (float *)output.payload.as_tensor.mutable_data_ptr();
+
+  auto data_addr = (float *)data;
+  auto memory_span_0_addr = (memory_span_0.data());
+  auto memory_span_1_addr = (memory_span_1.data());
+
+  std::cout << "memory_span_0_addr: " << std::hex << memory_span_0_addr
+            << std::endl;
+  std::cout << "memory_span_1_addr: " << std::hex << memory_span_1_addr
+            << std::endl;
+  std::cout << "data_addr: " << std::hex << data_addr << std::endl;
+  std::cout << "actual output_address: " << std::hex << output_address
+            << std::endl;
+
   std::cout << "output: " << std::endl;
+  torch::executor::etdump_result result = etdump_gen.get_etdump_data();
+  if (result.buf != nullptr && result.size > 0) {
+    // On a device with a file system users can just write it out
+    // to the file-system.
+    FILE *f = fopen("etdump.bin", "w+");
+    fwrite((uint8_t *)result.buf, 1, result.size, f);
+    fclose(f);
+    free(result.buf);
+  }
+
+  std::cout << "output: done" << std::endl; 
 }
